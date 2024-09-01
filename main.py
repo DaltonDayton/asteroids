9a584bf2c446f77ad7650befffdc03c272dfc49a
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-09-01 14:39:36 -0600

    add clock

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from constants import *
 
 
 def main():
-    pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -19,6 +21,8 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
